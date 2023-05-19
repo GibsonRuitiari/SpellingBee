9391b7bd2f82e8bdb8a8ecc6bba266cbe8fdef05
Ruitiari <gibsonruitiari@gmail.com>
2023-05-19 13:33:47 +0300

    removed redundant android-components

--- app/src/main/java/me/gibsoncodes/spellingbee/di/Factory.kt
+++ app/src/main/java/me/gibsoncodes/spellingbee/di/Factory.kt
@@ -9,10 +9,21 @@ import kotlin.properties.ReadOnlyProperty
 
 
 interface Factory<T> {
+    // instances Cache is a simple array map that stores objects created
+    // After a configuration change, instead of reconstructing new object instances
+    // the previously saved instances will be provided to the classes that need them
     val instancesCache:InstancesCache
+
+    /**
+     * The usage of CreationExtras is so that the factory implementations
+     * can be stateless hence injection of these factory classes is easier because
+     * they do not require any information during construction
+     * The key is a unique integer that is associated with the instance created
+     */
     fun createInstance(key:Int,
                        creationExtras:CreationExtras,
                        modelClass:Class<out T>):T
+    // gets the instance of object of type T if there is.
     fun get(key: Int,modelClass: Class<out T>,
             creationExtras: MutableCreationExtras):T?
     // to be called when the scope associated with the
@@ -20,7 +31,11 @@ interface Factory<T> {
     fun clear()
 }
 
+// default factory implementation, that provides some of the default implementation details,
+// of the factory interface other classes ought to use this when creating factories instead of directly implementing the
+// factory interface.
 abstract class DefaultFactory<T>:Factory<T> where T:Any{
+    // an id representing an object instance, to be set once get method is called
     private var internalId:Int? = null
     override fun get(key: Int,
                      modelClass: Class<out T>,
@@ -44,7 +59,8 @@ abstract class DefaultFactory<T>:Factory<T> where T:Any{
             removedValue?.closeQuietlyIfAutoCloseable()
         }
     }
-
+    // checks if the object to be constructed is an instance of AutoCloseable
+    // before removing
     private inline fun<reified T:Any>T.closeQuietlyIfAutoCloseable(){
         try {
             if (this::class.java.isInstance(AutoCloseable::class.java)){
@@ -92,6 +108,7 @@ object FactoryManager {
     }
 }
 
+// helper function for creating objects
 inline fun<reified T : Any> FactoryManager.create(key: Int,creationExtras:MutableCreationExtras=MutableCreationExtras(),
                                                   modelClass: Class<out T>,
                                                   noinline createObject:(key:Int,modelClass:Class<out T>,
