b0b62551afcc62cca123b1fcd03969a957c55531
GibsonRuitiari <gibsonruitiari@gmail.com>
2023-06-12 17:12:46 +0300

    refactored logging code

--- app/src/main/java/me/gibsoncodes/spellingbee/SpellingBeeApplication.kt
+++ app/src/main/java/me/gibsoncodes/spellingbee/SpellingBeeApplication.kt
@@ -1,9 +1,17 @@
 package me.gibsoncodes.spellingbee
 
 import android.app.Application
+import android.database.sqlite.SQLiteOpenHelper
 import android.os.HandlerThread
 import me.gibsoncodes.spellingbee.di.DefaultDependencyContainer
 import me.gibsoncodes.spellingbee.di.DependencyContainer
+import me.gibsoncodes.spellingbee.persistence.DatabaseHelper
+import me.gibsoncodes.spellingbee.persistence.PuzzleDao
+import me.gibsoncodes.spellingbee.persistence.PuzzleDaoDelegate
+import me.gibsoncodes.spellingbee.persistence.PuzzleRepository
+import me.gibsoncodes.spellingbee.persistence.PuzzleRepositoryDelegate
+import me.gibsoncodes.spellingbee.puzzlegenerator.PuzzleGenerator
+import me.gibsoncodes.spellingbee.puzzlegenerator.PuzzleGeneratorDelegate
 
 class SpellingBeeApplication :Application(){
     private val handlerThreadTag ="SpellingBeeHandlerThread"
@@ -16,5 +24,15 @@ class SpellingBeeApplication :Application(){
         super.onCreate()
         handlerThread= HandlerThread(handlerThreadTag).apply { start() }
         defaultDependencyContainer = DefaultDependencyContainer.getInstance()
+
+        defaultDependencyContainer.registerBinding(
+            SQLiteOpenHelper::class,
+            DatabaseHelper::class,applicationContext,BuildConfig.DatabaseName,BuildConfig.DatabaseVersion)
+        defaultDependencyContainer.registerBinding(
+            PuzzleDao::class,
+            PuzzleDaoDelegate::class, handlerThread)
+        defaultDependencyContainer.registerBinding(PuzzleRepository::class, PuzzleRepositoryDelegate::class)
+        defaultDependencyContainer.registerBinding(PuzzleGenerator::class, PuzzleGeneratorDelegate::class,handlerThread.looper, assets)
+
     }
 }
